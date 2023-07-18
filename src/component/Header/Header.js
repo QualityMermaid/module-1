4d165ba60352ee6ed9bd137fe7f9769536b477af
JessicaBane <jessicambane@gmail.com>
2023-07-18 16:34:13 +0100

    added cursor and sound

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,11 +1,22 @@
 "use Strict"
 
 import "./Header.css"
+import dragon from "../audio/dragon-roar-38093.mp3"
+
+// let audio = new Audio("../audio/dragon-roar-38093.mp3")
+let audio = new Audio(dragon)
+
 
 export default function Header(){
+    function playAudio(){
+    console.log("header clicked")
+        audio.play()
+    }
+    
     return(
         <header>
-            <h1> Beast World</h1>
+            {/* <h1 > Beast World</h1> */}
+            <button className="beastButton" onClick={playAudio}> <h1 > Beast World</h1></button>
         </header>
     )
 }
